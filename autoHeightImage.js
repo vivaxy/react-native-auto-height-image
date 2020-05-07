035e6a86ba39daf3c16d75d7467f925d94537023
vivaxy <xyxuye2007@126.com>
2020-05-07 15:16:50 +0800

    fix: :bug: fix `Can't perform a React state update on an unmounted component.`
    closes #44

--- autoHeightImage.js
+++ autoHeightImage.js
@@ -2,7 +2,7 @@
  * @since 2017-04-11 19:10:08
  * @author vivaxy
  */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ImagePolyfill from './imagePolyfill';
 import AnimatableImage from './animatableImage';
 import PropTypes from 'prop-types';
@@ -16,14 +16,27 @@ const { resizeMode, ...ImagePropTypes } = AnimatableImage.propTypes;
 function AutoHeightImage(props) {
   const { onHeightChange, source, width, style, ...rest } = props;
   const [height, setHeight] = useState(DEFAULT_HEIGHT);
+  const mountedRef = useRef(false);
 
-  useEffect(() => {
-    (async () => {
-      const { height: newHeight } = await getImageSizeFitWidth(source, width);
-      setHeight(newHeight);
-      onHeightChange(newHeight);
-    })();
-  }, [source, onHeightChange, width]);
+  useEffect(function() {
+    mountedRef.current = true;
+    return function() {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  useEffect(
+    function() {
+      (async function() {
+        const { height: newHeight } = await getImageSizeFitWidth(source, width);
+        if (mountedRef.current) {
+          setHeight(newHeight);
+          onHeightChange(newHeight);
+        }
+      })();
+    },
+    [source, onHeightChange, width]
+  );
 
   // StyleSheet.create will cache styles, not what we want
   const imageStyles = { width, height };
